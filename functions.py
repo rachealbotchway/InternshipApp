356ab03e7ba30a443319665c6cf0b0128cd234a5
Racheal Botchway <botchwayracheal020@gmail.com>
2024-01-10 01:22:31 +0000

    added function

---
+++ functions.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def wrangle(data, loan_device_id, unit, lag="4H"):
+    df=data[['loan_device_id','upper_threshold','lower_threshold','value','unit']]
+    mask = (df['unit'] == unit) & (df['loan_device_id'] == loan_device_id)
+    new_df = df.loc[mask]
+    y = new_df["value"]
+    y = y.resample("4H").mean().fillna(method='ffill')
+    return y 
